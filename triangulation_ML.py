b98bd5fbc5ef81d335f24d9f12667ffbcbe5bcd3
ovrprf <overproof9@gmail.com>
2021-05-23 20:44:37 +0300

    update requiremets.txt

---
+++ triangulation_ML.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+
+
+if __name__ == '__main__':
+    print('asd')
